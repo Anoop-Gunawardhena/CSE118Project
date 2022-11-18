973aaf5c51df7681dd772f51b09e221ca93a6e60
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2022-11-17 16:31:34 -0800

    Rick Milestone 1 Complete - Android Setup Issue #16

--- Navigation/app/src/main/java/com/example/navigation/MapsActivity.java
+++ Navigation/app/src/main/java/com/example/navigation/MapsActivity.java
@@ -43,9 +43,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Set initial map position to be at CSE Building
+        LatLng CSE_BUILDING = new LatLng(32.881809, -117.233460);
+        mMap.addMarker(new MarkerOptions().position(CSE_BUILDING).title("CSE Building"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CSE_BUILDING));
+
+        // Mark CSE Crosswalk
+        LatLng CSE_CROSSWALK = new LatLng(32.881854, -117.233143);
+        mMap.addMarker(new MarkerOptions().position(CSE_CROSSWALK).title("CSE Crosswalk"));
+
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+
     }
 }
