b869eeba651bdfad40181a6abe43f9b2fd28c6aa
Laurenz Baumgart <laurenz.baumgart@tum.de>
2022-11-22 22:02:52 -0500

    script

---
+++ pubnub_test2.py
@@ -0,0 +1,52 @@
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory, PNOperationType
+
+pnconfig = PNConfiguration()
+
+pnconfig.subscribe_key = 'sub-c-abee09f7-35bc-4433-b4ba-ba12da2c5028'
+pnconfig.publish_key = 'pub-c-f30c3204-e2ee-4907-9d2a-57f68b84a3e9'
+pnconfig.user_id = "my_custom_user_id"
+pubnub = PubNub(pnconfig)
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            print('Connected')     
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, pubnub, message):
+        # Handle new message stored in message.message
+        print(message.message)
+
+pubnub.add_listener(MySubscribeCallback())
+
+while True:
+    m = input("Your Input: ")
+    pubnub.publish().channel('my_channel').message(m).pn_async(my_publish_callback)   
