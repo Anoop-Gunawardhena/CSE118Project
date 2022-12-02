296c391988a13fdaedfb7bb11e58fb5707c034aa
Rick Vu Truong <ricktruong@Ricks-MacBook-Pro.local>
2022-12-01 20:39:12 -0800

    Completed Crosswalk Detection component of Android Application. Cleaned up and refactored code.

--- Navigation/app/src/main/java/com/example/navigation/Constants.java
+++ Navigation/app/src/main/java/com/example/navigation/Constants.java
@@ -1,6 +1,24 @@
 package com.example.navigation;
 
 public class Constants {
-    public static final float GEOFENCE_RADIUS_IN_METERS = (float) 10;
-    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS = Long.MAX_VALUE;
+
+    // Geofence constants
+    public static final float GEOFENCE_RADIUS_IN_METERS = (float) 50;
+
+    // PubNub Channel detail fields
+    public static final String UUID = "androidPubnub";
+
+    // Rick Channel details
+    public static final String rickChannelName = "androidToVoiceflowChannel";
+    public static final String rickPublishKey = "pub-c-985fb8a7-d30c-40b9-8532-77173422dd55";
+    public static final String rickSubscribeKey = "sub-c-b3862175-ffc5-4c24-9a0b-f238c09b2d26";
+
+    // Laurenz Channel details
+    public static final String laurenzChannelName = "Channel-Barcelona";
+    public static final String laurenzPublishKey = "pub-c-f30c3204-e2ee-4907-9d2a-57f68b84a3e9";
+    public static final String laurenzSubscribeKey = "sub-c-abee09f7-35bc-4433-b4ba-ba12da2c5028";
+
+    // PubNub messages fields
+    public static final String CROSSWALK_DETECTED = "crosswalk detected";
+
 }
--- Navigation/app/src/main/java/com/example/navigation/GeofenceBroadcastReceiver.java
+++ Navigation/app/src/main/java/com/example/navigation/GeofenceBroadcastReceiver.java
@@ -1,38 +1,248 @@
 package com.example.navigation;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.PubNubException;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNPublishResult;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.objects_api.channel.PNChannelMetadataResult;
+import com.pubnub.api.models.consumer.objects_api.membership.PNMembershipResult;
+import com.pubnub.api.models.consumer.objects_api.uuid.PNUUIDMetadataResult;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+import com.pubnub.api.models.consumer.pubsub.PNSignalResult;
+import com.pubnub.api.models.consumer.pubsub.files.PNFileEventResult;
+import com.pubnub.api.models.consumer.pubsub.message_actions.PNMessageActionResult;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * GEOFENCE BROADCAST RECEIVER CLASS
+ *
+ * Includes MESSAGE PUBLICATION POST-CROSSWALK DETECTED feature
+ */
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    /** PUBNUB INSTANCE VARIABLES **/
+    // PubNub connection instance field
+    private PubNub pubnub;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        // Get Geofence Event onReceive Intent
         GeofencingEvent event = GeofencingEvent.fromIntent(intent);
-        String transition = mapTransition(event.getGeofenceTransition());
-
-        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new NotificationCompat.Builder(context)
-                .setContentTitle("Geofence action")
-                .setContentText(transition)
-                .setTicker("Geofence action")
-                .build();
-        nm.notify(0, notification);
-    }
-
-    private String mapTransition(int event) {
-        switch (event) {
-            case Geofence.GEOFENCE_TRANSITION_ENTER:
-                return "ENTER";
-            case Geofence.GEOFENCE_TRANSITION_EXIT:
-                return "EXIT";
-            default:
-                return "UNKNOWN";
+        if (event.hasError()) {
+            System.out.println("onReceive: " + GeofenceStatusCodes.getStatusCodeString(event.getErrorCode()));
+            return;
         }
+
+        // Print out all triggering Geofences of event
+        for (Geofence geofence: event.getTriggeringGeofences()) {
+            System.out.println("onReceive: " + geofence.getRequestId());
+        }
+
+        // Determine Geofence Transition and perform transition
+        mapTransition(context, event.getGeofenceTransition());
+
     }
+
+    /**
+     * MESSAGE PUBLICATION POST-CROSSWALK DETECTED FEATURE
+     *
+     * Publish PubNub message to start Voiceflow workflow if user is proximal to a crosswalk (GEOFENCE_TRANSITION_ENTER is a success)
+     */
+    private void mapTransition(Context context, int transition) {
+
+        switch (transition) {
+
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_ENTER", Toast.LENGTH_SHORT).show();
+                System.out.println("onReceive: GEOFENCE TRANSITION ENTER detected. Publishing CROSSWALK_DETECTED message to " + Constants.laurenzChannelName);
+                pubnubMain();                           // PUBNUB PUBLICATION
+                break;
+
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_EXIT", Toast.LENGTH_SHORT).show();
+                System.out.println("onReceive: GEOFENCE TRANSITION EXIT detected");
+                break;
+
+            default:
+                Toast.makeText(context, "GEOFENCE_TRANSITION_UNKNOWN", Toast.LENGTH_SHORT).show();
+                System.out.println("onReceive: " + transition + " Geofence transition code detected. Unknown Action. Pass.");
+                break;
+
+        }
+
+    }
+
+    /**
+     * PUBNUB HELPER METHODS
+     */
+
+    /**
+     * PubNub connection and publication Main Helpher Method
+     */
+    public void pubnubMain() {
+
+        try {
+
+            /**
+             * PUBNUB CONNECTION SETUP
+             */
+            // Instantiate PubNub
+            pubnub = createPubNubConnection();
+
+            // Add subscribeCallBack listener to pubnub client
+            pubnub.addListener(createSubscribeCallback());
+
+            /**
+             * PUBNUB PUBLISH/SUBSCRIBE
+             */
+            // Publish CROSSWALK_DETECTED message
+            pubnub.publish().message(Constants.CROSSWALK_DETECTED).channel(Constants.laurenzChannelName).async(createPNCallback());
+
+            // Subscribe to androidToVoiceflowChannel to receive CROSSWALK_DETECTED message
+            pubnub.subscribe().channels(Arrays.asList(Constants.laurenzChannelName)).execute();
+
+        } catch (PubNubException e) {
+            System.out.println("*** EXCEPTION IN PUBNUB CONNECTION ***");
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Helper method to create PubNub Connection object which establishes connection to PubNub channel
+     */
+    public PubNub createPubNubConnection() throws PubNubException {
+
+        PNConfiguration pnConfiguration = new PNConfiguration(Constants.UUID);
+        pnConfiguration.setSubscribeKey(Constants.laurenzSubscribeKey);
+        pnConfiguration.setPublishKey(Constants.laurenzPublishKey);
+
+        return new PubNub(pnConfiguration);
+
+    }
+
+    /**
+     * create PubNub Subscribe Callback object helper method
+     */
+    public SubscribeCallback createSubscribeCallback() {
+
+        SubscribeCallback subscribeCallback = new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+
+                if (status.getCategory() == PNStatusCategory.PNUnexpectedDisconnectCategory) {
+                    pubnub.reconnect();
+                }
+                else if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+                    System.out.println("PubNub Android client connected to: " + Constants.laurenzChannelName);
+                }
+                else if (status.getCategory() == PNStatusCategory.PNReconnectedCategory) {
+                    System.out.println("PubNub Android client reconnected to: " + Constants.laurenzChannelName);
+                }
+                else if (status.getCategory() == PNStatusCategory.PNDecryptionErrorCategory) {
+                    pubnub.reconnect();
+                }
+
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+
+                // Handle new message stored in message.message
+                String messagePublisher = message.getPublisher();
+                System.out.println("Message publisher: " + messagePublisher);
+                System.out.println("Message Subscription: " + message.getSubscription());
+                System.out.println("Message Channel: " + message.getChannel());
+                System.out.println("Message: " + message.getMessage());
+
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+            }
+
+            @Override
+            public void signal(@NotNull PubNub pubNub, @NotNull PNSignalResult pnSignalResult) {
+            }
+
+            @Override
+            public void uuid(@NotNull PubNub pubNub, @NotNull PNUUIDMetadataResult pnuuidMetadataResult) {
+            }
+
+            @Override
+            public void channel(@NotNull PubNub pubNub, @NotNull PNChannelMetadataResult pnChannelMetadataResult) {
+            }
+
+            @Override
+            public void membership(@NotNull PubNub pubNub, @NotNull PNMembershipResult pnMembershipResult) {
+            }
+
+            @Override
+            public void messageAction(@NotNull PubNub pubNub, @NotNull PNMessageActionResult pnMessageActionResult) {
+            }
+
+            @Override
+            public void file(@NotNull PubNub pubNub, @NotNull PNFileEventResult pnFileEventResult) {
+            }
+
+        };
+
+        return subscribeCallback;
+    }
+
+    /**
+     * create PubNub Callback object helper method
+     * @return
+     */
+    public PNCallback<PNPublishResult> createPNCallback() {
+
+        PNCallback<PNPublishResult> pnCallback = new PNCallback<PNPublishResult>() {
+
+            @Override
+            public void onResponse(PNPublishResult result, PNStatus status) {
+
+                // Check whether request successfully completed or not.
+                if (!status.isError()) {
+                    // Message successfully published to specified channel.
+                    System.out.println("Message successfully published to specified channel");
+                }
+
+                // Request processing failed.
+                else {
+                    status.retry();
+                }
+
+            }
+
+        };
+
+        return pnCallback;
+
+    }
+
 }
