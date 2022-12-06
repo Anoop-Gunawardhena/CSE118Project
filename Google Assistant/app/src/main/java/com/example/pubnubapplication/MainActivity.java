4aaf5c50ee79ac1ed88902e5f643e3da1ea19f62
AD\ax008372 <laurenz.baumgart@TUM.DE>
2022-12-05 17:02:43 -0800

    Refactor TTS and Recognizer

--- Google Assistant/app/src/main/java/com/example/pubnubapplication/MainActivity.java
+++ Google Assistant/app/src/main/java/com/example/pubnubapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.pubnubapplication;
 
 
-import static android.content.pm.PackageManager.PERMISSION_DENIED;
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
-
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,7 +9,6 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import android.speech.tts.TextToSpeech;
 //import android.widget.Toast;
@@ -21,14 +17,12 @@ import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.pubnubapplication.databinding.ActivityMainBinding;
-import com.google.android.material.snackbar.Snackbar;
 
 import com.pubnub.api.PNConfiguration;
 import com.pubnub.api.PubNub;
@@ -62,24 +56,24 @@ public class MainActivity extends AppCompatActivity {
 
     private SpeechRecognizer speechRecognizer;
 
-    TextToSpeech tts1;
+    TextToSpeech tts;
     void output(String toSpeak) {
-        tts1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int ttsStatus) {
                 if (ttsStatus != TextToSpeech.ERROR) {
-                    tts1.setLanguage(Locale.US);
+                    tts.setLanguage(Locale.US);
                     //System.out.println("I have initialized!");
-                    tts1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+                    tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
                 } else {
                     System.out.println(ttsStatus);
                 }
             }
 
             public void onPause() {
-                if (tts1 != null) {
-                    tts1.stop();
-                    tts1.shutdown();
+                if (tts != null) {
+                    tts.stop();
+                    tts.shutdown();
                 }
             }
         }, "com.google.android.tts");
@@ -137,9 +131,9 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResults(Bundle bundle) {
                     ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                    System.out.println(data.get(0));
+
                     if (data.get(0).equals("yes")) {
-                        System.out.println("Stop");
+                        System.out.println("User answered \"Yes\"");
                         speechRecognizer.stopListening();
                         pubNub.publish()
                                 .message(Arrays.asList("record"))
@@ -149,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                                     public void onResponse(PNPublishResult result, PNStatus status) {
                                         // handle publish result, status always present, result if successful
                                         // status.isError to see if error happened
-                                        System.out.println("Success sending" + result);
+                                        System.out.println("Success sending: " + result);
                                         System.out.println(status);
                                     }
                                 }); //change yes stuff to send message to pi, create a no thing, implement listening logic from correct message
@@ -162,7 +156,8 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onPartialResults(Bundle bundle) {
-
+                    ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                    System.out.println(data.get(0));
 
                 }
 
@@ -191,44 +186,26 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(message.getMessage().toString());
                     // Let the Google Assistant ouput the message as soon as it arrives
                     // Pseudo code: GoogleAssistant.speak(message.getMessage()
-                    if(message.getMessage().toString().equals("[\"record\"]")) {
-                        System.out.println("sent message to pi");
-                    }
-                    else if(message.getMessage().toString().equals("[\"crossing now\"]")) {
-                        System.out.println("sent message that user is crossing");
-                    }
-                    else if(message.getMessage().toString().equals("[\"crosswalk detected\"]")) {
+
+
+                    if(message.getMessage().toString().equals("\"crosswalk detected\"")) {
+                        //TODO: Move this to the part where Rick detects a crosswalk with his geofence
                         System.out.println("sending message to watch");
                         output("crosswalk detected, would you like to cross?");
-                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+
+                        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 //this runs on the UI thread
                                 openMic();
                             }
-                        });
+                        }, 3000);
                     }
+                    //Message from the PI
                     else if(message.getMessage().toString().equals("[\"safe to cross\"]")) {
-                        //System.out.println("sending message to watch");
-                        output("It is safe to cross now");
-                        pubNub.publish()
-                                .message(Arrays.asList("crossing now"))
-                                .channel("Channel-Barcelona")
-                                .async(new PNCallback<PNPublishResult>() {
-                                    @Override
-                                    public void onResponse(PNPublishResult result, PNStatus status) {
-                                        // handle publish result, status always present, result if successful
-                                        // status.isError to see if error happened
-                                        System.out.println("Success sending" + result);
-                                        System.out.println(status);
-                                    }
-                                });
+                        //TODO: Rick should activate the Walking straight feature here
+                        output("There are no cars detected, you can cross now");
                     }
-                    else {
-                        output(message.getMessage().toString());
-                    }
-                    //String toSpeak = message.getMessage().toString();
-                    //tts1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
                 }
 
                 @Override
@@ -287,29 +264,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //all the stuff for this button is for testing purposes
-                /*final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-                speechRecognizer.startListening(speechRecognizerIntent); */
-                pubNub.publish()
-                        .message(Arrays.asList("safe to cross"))
-                        .channel("Channel-Barcelona")
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
-                                // handle publish result, status always present, result if successful
-                                // status.isError to see if error happened
-                                System.out.println("Success sending" + result);
-                                System.out.println(status);
-                            }
-                        });
-            }
-        });
     }
 
     @Override
